699c7b828f1b5405e2fe63387be4bc477eff3cab
Alexis Morin <carignan.boy@gmail.com>
2013-02-27 14:57:01 +0100

    verbs return to infinitive form when removed from sentence

--- src/com/alexismorin/laps/Scene.java
+++ src/com/alexismorin/laps/Scene.java
@@ -1,5 +1,6 @@
 package com.alexismorin.laps;
 
+import com.alexismorin.laps.grammar.Conjugable;
 import com.alexismorin.laps.grammar.Grammar;
 import com.alexismorin.laps.grammar.IsArticle;
 import com.alexismorin.laps.grammar.LinkTo;
@@ -33,6 +34,11 @@ public class Scene {
 								// simplifies instanciation
 				w.pos = new PVector(100 * (i + 1) + 20, 100);
 			}
+			
+			if(!w.snapped && w instanceof Conjugable){
+				w.setWord(((Conjugable)w).conjugate(0, "infinitive"));
+			}
+			
 		}
 	}
 
@@ -54,59 +60,57 @@ public class Scene {
 		parent.rectMode(parent.CORNER);
 		parent.textFont(ubuntu, 24);
 
-		//not useful for now
+		// not useful for now
 		Sentence sentence = board.makeSentence();
 		sentence.checkSentenceGrammar();
-		
+
 		for (int i = 0; i < board.words.size(); i++) {
 			Word w = board.words.get(i);
-			
-			parent.stroke(0);//paint word outline the default color: black
-			
+
+			parent.stroke(0);// paint word outline the default color: black
+
 			if (w.snapped) {
 				Word nextW = board.words.getNextSnapped(i);
 				Word prevW = board.words.getPrevSnapped(i);
-				if(w.do_grammar(prevW, nextW)){
+				if (w.do_grammar(prevW, nextW)) {
 					parent.stroke(38, 133, 36);// paint it green for correct grammar
-					//draw a bezier, like a bawss
-					if(w instanceof LinkTo){
-						drawBezier(w, ((LinkTo)w).linkToWord());
+					// draw a bezier, like a bawss
+					if (w instanceof LinkTo) {
+						drawBezier(w, ((LinkTo) w).linkToWord());
 					}
-					
-				}else {
+
+				} else {
 					parent.stroke(205, 24, 24);// paint it red for bad grammar
 				}
-			} 
-			
+			}
+
 			// finish drawing the tile
 			parent.fill(200);
 			parent.rect(w.pos.x, w.pos.y, Config.wordSize, Config.wordSize);
 
 			// draw the words from the tiles
 			parent.fill(0);
-			parent.text(w.getWord(), w.pos.x + 10, w.pos.y + Config.wordSize/ 2);
+			parent.text(w.getWord(), w.pos.x + 10, w.pos.y + Config.wordSize
+					/ 2);
 			// parent.text(String.valueOf(i), w.pos.x + 10, w.pos.y +
 			// Config.wordSize / 3);
 
-			// draw the Sentence
-			
 		}
-		
-		parent.text(board.getSentenceString(), 
-				parent.width / 2,
+		// draw the Sentence
+		parent.text(board.getSentenceString(), parent.width / 2,
 				parent.height - 100);
 	}
 
-	void drawBezier(Word w1, Word w2){
-		//center of the 1st word
+	void drawBezier(Word w1, Word w2) {
+		// center of the 1st word
 		parent.noFill();
-		parent.bezier(w1.pos.x+Config.wordSize/2, w1.pos.y+Config.wordSize/2, 
-				w1.pos.x+Config.wordSize/2, w1.pos.y-Config.wordSize, 
-				w2.pos.x+Config.wordSize/2, w2.pos.y-Config.wordSize, 
-				w2.pos.x+Config.wordSize/2, w2.pos.y+Config.wordSize/2
-				);
+		parent.bezier(w1.pos.x + Config.wordSize / 2, w1.pos.y
+				+ Config.wordSize / 2, w1.pos.x + Config.wordSize / 2, w1.pos.y
+				- Config.wordSize, w2.pos.x + Config.wordSize / 2, w2.pos.y
+				- Config.wordSize, w2.pos.x + Config.wordSize / 2, w2.pos.y
+				+ Config.wordSize / 2);
 	}
-	
+
 	void onMouseEvent(MouseEvent e) {
 		board.onMouseEvent(e);
 	}
